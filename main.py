c6d16bd2266265cc2a1b00610e0e7dfa0a1db09e
André Freitas <p.andrefreitas@gmail.com>
2014-11-29 17:15:52 +0000

    Resolvido um problema da imagem banalizada de que não aparecia a correta.

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import copy
 img = cv2.imread('images/image.jpg')
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY)
-_ , contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-img_contours = copy.copy(img)
-cv2.drawContours(img_contours, contours, -1, (0, 255, 0), 3)
+_ , contours, _ = cv2.findContours(copy.copy(thresh), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+img_contours = cv2.drawContours(copy.copy(img), contours, -1, (0, 255, 0), 3)
 
 cv2.imshow('Imagem Original', img)
 cv2.imshow('Imagem Binarizada', thresh)
